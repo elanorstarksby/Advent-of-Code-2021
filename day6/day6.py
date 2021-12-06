17695ccc27845a7caa1b707734b88bbbd57cf3d2
Elanor Stark <elanor.stark@sainsburys.co.uk>
2021-12-06 18:10:46 +0000

    Tidy up day 6

--- day6/day6.py
+++ day6/day6.py
@@ -1,5 +1,4 @@
 def read_file():
-    values = []
     with open("input_day6.txt", "r") as input_file:
         values = [int(i) for i in input_file.readline().strip().split(",")]
     return values
@@ -19,23 +18,14 @@ def progress(model):
 def after_days(model, days):
     for day in range(days):
         model = progress(model)
-        # print(day,model)
     return model
 
 
 def calculate(model, days):
     first_grows = [0, 0, 0, 0, 0, 0, 0]
-    waiting = [[0, 0] for _ in first_grows]
 
     for fish in model:
         first_grows[fish] += 1
-
-    # for i in range(days):
-    #     print("b", i, first_grows, waiting)
-    #     first_grows[i % 7] += waiting[i % 7][0]
-    #     waiting[i % 7][0] = waiting[i % 7][1]
-    #     waiting[(i + 1) % 7][1] = first_grows[i % 7]
-    #     print("a", day % 7, first_grows, waiting)
 
     to_add_on_days = {}
 
@@ -49,11 +39,11 @@ def calculate(model, days):
             to_add_on_days.pop(day)
 
         # print(day, first_grows, to_add_on_days)
-    print(sum(first_grows)+ sum(to_add_on_days.values()))
+    return sum(first_grows)+ sum(to_add_on_days.values())
 
 
 if __name__ == '__main__':
     fish_model = read_file()
     print(len(after_days(fish_model, 80)))
     fish_model = read_file()
-    calculate(fish_model, 256)
+    print(calculate(fish_model, 256))
